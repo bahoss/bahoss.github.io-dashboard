22b28eaf5a0062acc792b49fdbd1fba110c44f9a
bahoss <bahobek@gmail.com>
2019-01-15 12:12:22 +0600

    commit1.2

--- src/components/Blocks/Block.styled.js
+++ src/components/Blocks/Block.styled.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Block = styled.div`
   display: flex;
   flex-direction: column;
-  margin: 10px;
+  padding: 10px;
 `;
 export const Span = styled.span`
   display: iline-block;
