f4acab943c6c853408e0c60d713de4481268b341
bahoss <bahobek@gmail.com>
2019-01-17 16:39:31 +0600

    commit 3.1

--- src/components/BlockList/index.js
+++ src/components/BlockList/index.js
@@ -4,23 +4,106 @@ import { connect } from "react-redux";
 import { selectData } from "../../Entities/Systems/selectors";
 import { getData } from "../../Entities/Systems/actions";
 import { Wrapper } from "./BlockList.styled";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+
+// fake data generator
+
+// a little function to help us with reordering the result
+const reorder = (list, startIndex, endIndex) => {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+
+  return result;
+};
+
+const grid = 8;
+
+const getItemStyle = (isDragging, draggableStyle) => ({
+  // some basic styles to make the items look a bit nicer
+  userSelect: "none",
+  padding: grid * 2,
+  margin: `0 ${grid}px 0 0`,
+
+  // change background colour if dragging
+  background: isDragging ? "lightgreen" : "grey",
+
+  // styles we need to apply on draggables
+  ...draggableStyle
+});
+
+const getListStyle = isDraggingOver => ({
+  background: isDraggingOver ? "lightblue" : "lightgrey",
+  display: "flex",
+  padding: grid,
+  overflow: "auto"
+});
 
 class BlockList extends Component {
-  getData = () => {
-    this.props.getData();
-  };
-
-  showData = () => {
-    return this.props.dataItem.map(item => <Block key={item.id} res={item} />);
-  };
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: this.props.dataItem
+    };
+    this.onDragEnd = this.onDragEnd.bind(this);
+  }
   componentDidMount() {
     this.props.getData();
     setInterval(this.props.getData, 20000);
   }
 
+  onDragEnd(result) {
+    // dropped outside the list
+    if (!result.destination) {
+      return;
+    }
+
+    const items = reorder(
+      this.state.items,
+      result.source.index,
+      result.destination.index
+    );
+
+    this.setState({
+      items
+    });
+  }
+
+  // Normally you would want to split things out into separate components.
+  // But in this example everything is just done in one place for simplicity
   render() {
-    return <Wrapper>{this.showData()}</Wrapper>;
+    return (
+      <DragDropContext onDragEnd={this.onDragEnd}>
+        <Droppable droppableId="droppable" direction="horizontal">
+          {(provided, snapshot) => (
+            <div
+              ref={provided.innerRef}
+              style={getListStyle(snapshot.isDraggingOver)}
+              {...provided.droppableProps}
+            >
+              {this.state.items.map((item, index) => (
+                <Draggable key={item.id} draggableId={item.id} index={index}>
+                  {(provided, snapshot) => (
+                    <div
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                      style={getItemStyle(
+                        snapshot.isDragging,
+                        provided.draggableProps.style
+                      )}
+                    >
+                      {item.content}
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
+    );
   }
 }
 
--- src/store/rootSaga.js
+++ src/store/rootSaga.js
@@ -1,10 +1,7 @@
-import { fork } from 'redux-saga/effects';
-import fetchSaga from '../components/Router/sagas';
-import patchSaga from '../components/Comments/sagas';
+import { fork } from "redux-saga/effects";
+import fetchSaga from "../components/BlockList/sagas";
+import patchSaga from "../components/Comments/sagas";
 
 export default function* rootSaga() {
-    yield [
-        fork(fetchSaga),
-        fork(patchSaga)
-    ];
+  yield [fork(fetchSaga), fork(patchSaga)];
 }
